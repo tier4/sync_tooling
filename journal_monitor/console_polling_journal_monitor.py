b6c584f2511bef42ddddd794974b51f8fdd362de
Max SCHMELLER <max.schmeller@tier4.jp>
2024-12-19 11:35:25 +0900

    feat(journal_monitor): add a polling-based implementation that invokes `journalctl`
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

---
+++ journal_monitor/console_polling_journal_monitor.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+import json
+import subprocess
+from typing import Dict, Set
+
+from journal_monitor.journal_monitor import JournalEntry, JournalMonitor
+
+
+def json_to_journal_entry(json: dict):
+    system_timestamp_us = int(json["__REALTIME_TIMESTAMP"])
+    system_timestamp_us = datetime.fromtimestamp(system_timestamp_us / 1e6)
+    monotonic_timestamp_us = int(json["__MONOTONIC_TIMESTAMP"])
+    cursor = json["__CURSOR"]
+    unit = json["_SYSTEMD_UNIT"]
+    message = json.get("MESSAGE")
+
+    return JournalEntry(
+        system_timestamp_us, monotonic_timestamp_us, cursor, unit, message
+    )
+
+
+class ConsolePollingJournalMonitor(JournalMonitor):
+    def __init__(self):
+        super().__init__()
+        self._flags: Set[str] = {"--output=json"}
+        self._previous_cursor: str | None = None
+
+    def only_current_boot(self) -> JournalMonitor:
+        self._flags.add("--boot")
+        return self
+
+    def only_systemd_unit(self, unit_name: str) -> JournalMonitor:
+        self._flags = {f for f in self._flags if not f.startswith("--unit=")}
+        self._flags.add(f"--unit={unit_name}")
+        return self
+
+    def poll(self):
+        args = ["journalctl"]
+        args += self._flags
+        if self._previous_cursor is not None:
+            args.append(f"--after-cursor={self._previous_cursor}")
+        output = subprocess.check_output(args)
+        output = output.decode()
+        json_strings = output.splitlines()
+        json_objs: list[Dict] = [json.loads(s) for s in json_strings]
+        journal_entries: list[JournalEntry] = [
+            json_to_journal_entry(j) for j in json_objs
+        ]
+
+        if journal_entries:
+            self._previous_cursor = journal_entries[-1].cursor
+        return journal_entries
