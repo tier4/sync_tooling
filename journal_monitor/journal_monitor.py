c1e4f7cc031d8cc538eb490a8f0035ff376b893a
Max SCHMELLER <max.schmeller@tier4.jp>
2024-12-19 11:34:51 +0900

    feat(journal_monitor): add an abstract class for monitoring the systemd journal
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

---
+++ journal_monitor/journal_monitor.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from datetime import datetime
+from typing import List
+
+
+@dataclass
+class JournalEntry:
+    system_timestamp: datetime
+    monotonic_timestamp_us: int
+    cursor: str
+    unit: str
+    message: str | None
+
+
+class JournalMonitor(ABC):
+    """
+    Provides an interface to subscribe to systemd journal entries.
+    """
+
+    @abstractmethod
+    def only_current_boot(self) -> "JournalMonitor":
+        raise NotImplementedError()
+
+    @abstractmethod
+    def only_systemd_unit(self, unit_name: str) -> "JournalMonitor":
+        raise NotImplementedError()
+
+    @abstractmethod
+    def poll(self) -> List[JournalEntry]:
+        raise NotImplementedError()
