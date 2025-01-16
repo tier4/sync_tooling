4406e6b05eae3ff5c30ba42476fdef2e221be96a
Max SCHMELLER <max.schmeller@tier4.jp>
2025-01-16 23:44:06 +0900

    feat(diag_master): add rudimentary diag_master implementation
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- diag_master/src/diag_master/__init__.py
+++ diag_master/src/diag_master/__init__.py
@@ -1,2 +1,44 @@
-def main() -> None:
-    print("Hello from diag-master!")
+import socket
+
+from sync_graph import SyncGraph
+
+import diag_tree
+
+import rclpy
+import rclpy.qos
+from ros2_transport import JsonSubscription
+
+
+class DiagMaster:
+    def __init__(self) -> None:
+        hostname = socket.gethostname()
+        if not hostname:
+            raise RuntimeError("Could not determine hostname")
+
+        self.node = rclpy.create_node(f"/sync_diag/master/{hostname}")  # type: ignore
+        self.subscription_ = JsonSubscription(
+            self.node,
+            "/sync_diag/graph_updates",
+            10,
+            self.json_callback,
+            self.error_callback,
+            [diag_tree.Unknown, diag_tree.Ok, diag_tree.Warning, diag_tree.Error],
+        )
+
+        self.sync_graph_ = SyncGraph()
+
+    def run(self):
+        rclpy.spin(self.node)
+
+    def error_callback(self, err):
+        self.node.get_logger().error(f"Could not parse received graph update: {err}")
+
+    def json_callback(self, j):
+        pass
+
+
+def main():
+    rclpy.init()
+
+    diag_master = DiagMaster()
+    diag_master.run()
