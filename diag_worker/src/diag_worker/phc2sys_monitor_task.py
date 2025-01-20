45080c1de0257f119df22b7cf4a3ac1d5b76922e
Max SCHMELLER <max.schmeller@tier4.jp>
2025-01-20 19:41:04 +0900

    chore(diag_worker): publish parsed graph updates
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- diag_worker/src/diag_worker/phc2sys_monitor_task.py
+++ diag_worker/src/diag_worker/phc2sys_monitor_task.py
@@ -1,7 +1,7 @@
 from sync_graph import Clock, ClockUpdate, Phc2SysUpdate
 from diag_tree import diagnose
 from diag_worker.monitor_task import MonitorTask
-from diag_worker.systemd_util import get_command_line, get_unit_pid
+from diag_worker.systemd_util import does_unit_exist, get_command_line, get_unit_pid
 from diag_worker.util import linuxptp_to_graph_clock_id
 from journal_monitor.console_polling_journal_monitor import ConsolePollingJournalMonitor
 from linuxptp_monitor.phc2sys_instance import Phc2SysConfig, Phc2SysRunningState
@@ -13,11 +13,17 @@ from linuxptp_monitor.state_machine import (
 
 class Phc2SysMonitorTask(MonitorTask):
     def __init__(self, unit_name: str, hostname: str):
+        if not does_unit_exist(unit_name):
+            raise FileNotFoundError(f"Unit {unit_name} was not found on this system")
+
         self.journal_monitor = (
             ConsolePollingJournalMonitor()
             .only_current_boot()
             .only_systemd_unit(unit_name)
         )
+
+        self.unit_name_ = unit_name
+        self.hostname_ = hostname
 
         def phc2sys_factory():
             pid = get_unit_pid(unit_name)
@@ -31,7 +37,9 @@ class Phc2SysMonitorTask(MonitorTask):
         self.state_machine = SystemdUnitStateMachine(
             phc2sys_factory, on_stopped, unit_name
         )
-        self.hostname_ = hostname
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}(hostname={self.hostname_}, unit={self.unit_name_})"
 
     def to_graph_updates(self, state_change: SystemdUnitStateChange):
         Uninitialized = SystemdUnitStateMachine.Uninitialized
@@ -67,5 +75,6 @@ class Phc2SysMonitorTask(MonitorTask):
         for entry in journal_entries:
             state_change = self.state_machine.consume(entry)
             if state_change is not None:
+                print(f"got state change: {state_change.__class__.__name__}")
                 for update in self.to_graph_updates(state_change):
                     yield update
--- diag_worker/src/diag_worker/ptp4l_monitor_task.py
+++ diag_worker/src/diag_worker/ptp4l_monitor_task.py
@@ -1,6 +1,6 @@
 from sync_graph import ClockAliasUpdate, ClockId, PtpClockId
 from diag_worker.monitor_task import MonitorTask
-from diag_worker.systemd_util import get_command_line, get_unit_pid
+from diag_worker.systemd_util import does_unit_exist, get_command_line, get_unit_pid
 from diag_worker.util import linuxptp_to_graph_clock_id
 from journal_monitor.console_polling_journal_monitor import ConsolePollingJournalMonitor
 from linuxptp_monitor.ptp4l_instance import (
@@ -13,12 +13,16 @@ from pmc_monitor.pmc_monitor import PmcMonitor, PmcStateChange, PtpInstance
 
 class Ptp4lMonitorTask(MonitorTask):
     def __init__(self, unit_name: str, hostname: str):
+        if not does_unit_exist(unit_name):
+            raise FileNotFoundError(f"Unit {unit_name} was not found on this system")
+
         self.journal_monitor = (
             ConsolePollingJournalMonitor()
             .only_current_boot()
             .only_systemd_unit(unit_name)
         )
 
+        self.unit_name_ = unit_name
         self.hostname_ = hostname
         self.pmc_monitor: PmcMonitor | None = None
         self.ptp4l_clock_id: ClockId | None = None
@@ -40,6 +44,9 @@ class Ptp4lMonitorTask(MonitorTask):
         self.state_machine = SystemdUnitStateMachine(
             ptp4l_state_factory, on_stopped, unit_name
         )
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}(hostname={self.hostname_}, unit={self.unit_name_})"
 
     def _create_pmc_monitor(self, config: Ptp4lConfig):
         self._reset_pmc_monitor()
@@ -65,11 +72,14 @@ class Ptp4lMonitorTask(MonitorTask):
             state_change = self.state_machine.consume(entry)
             if state_change is not None:
                 # TODO(mojomex): decide what info is needed from PTP4L
+                print(f"got event: {state_change.__class__.__name__}")
                 pass
 
         if self.pmc_monitor is not None:
             async for event in self.pmc_monitor.poll():
-                yield event
+                print(f"got event: {event.__class__.__name__}")
+                for graph_update in self.pmc_to_graph_updates(event):
+                    yield graph_update
 
     def stop(self):
         super().stop()
