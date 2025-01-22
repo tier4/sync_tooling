deecae3374e38fc9124bacc922c7da687be30dd5
Max SCHMELLER <max.schmeller@tier4.jp>
2025-01-22 19:06:45 +0900

    fix(tcp_transport): correctly decode non-dataclass fields within dataclass

--- tcp_transport/src/tcp_transport/__init__.py
+++ tcp_transport/src/tcp_transport/__init__.py
@@ -44,57 +44,14 @@ class DataclassJsonEncoder(json.JSONEncoder):
         }
 
 
-class JsonPublisher:
-    def __init__(
-        self, peer_name: str, master_ip: str, master_port: int
-    ) -> None:
-        self.sock_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-        self.sock_.connect((master_ip, master_port))  
+class DataclassJsonDecoder(json.JSONDecoder):
+    def __init__(self, known_types: Iterable[Type | UnionType]) -> None:
+        super().__init__(object_hook=self.object_hook_)
 
-    def publish(self, obj):
-        serialized = json.dumps(
-            obj,
-            cls=DataclassJsonEncoder,
-        )
-        self.sock_.send(serialized.encode())
-
-class JsonSubscription:
-    def __init__(
-        self,
-        bind_ip: str,
-        bind_port: int,
-        json_callback: Callable[[Any], None],
-        known_types: Iterable[Type | UnionType],
-    ) -> None:
         known_dataclasses = set()
         for typ in known_types:
             known_dataclasses |= get_dataclasses_transitive(typ)
-
-        self.json_callback_ = json_callback
         self.known_dataclasses_ = {t.__qualname__: t for t in known_dataclasses}
-        
-        self.bind_ip_ = bind_ip
-        self.bind_port_ = bind_port
-
-        self.json_decoder_ = json.JSONDecoder(object_hook=self.object_hook_)
-
-
-    async def handle_client_(self, reader: asyncio.StreamReader, _):
-        request = ""
-        while True:
-            request += (await reader.read(1024)).decode('utf8')
-            try:
-                j, end = self.json_decoder_.raw_decode(request)
-            except json.JSONDecodeError:
-                continue
-            else:
-                self.json_callback_(j)
-                request = request[end:]
-
-    async def listen(self):
-        server = await asyncio.start_server(self.handle_client_, self.bind_ip_, self.bind_port_)
-        async with server:
-            await server.serve_forever()
 
     def object_hook_(self, o: dict[str, Any]):
         if "__dataclass__" in o:
@@ -105,4 +62,57 @@ class JsonSubscription:
         class_qualname = o["__dataclass__"]
         cls = self.known_dataclasses_[class_qualname]
         del o["__dataclass__"]
-        return cls(**o)
+        return cls(
+            **{
+                k: self.decode(v) if not dataclasses.is_dataclass(v) else v
+                for k, v in o.items()
+            }
+        )
+
+
+class JsonPublisher:
+    def __init__(self, peer_name: str, master_ip: str, master_port: int) -> None:
+        self.sock_ = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock_.connect((master_ip, master_port))
+
+    def publish(self, obj):
+        serialized = json.dumps(
+            obj,
+            cls=DataclassJsonEncoder,
+        )
+        self.sock_.send(serialized.encode())
+
+
+class JsonSubscription:
+    def __init__(
+        self,
+        bind_ip: str,
+        bind_port: int,
+        json_callback: Callable[[Any], None],
+        known_types: Iterable[Type | UnionType],
+    ) -> None:
+        self.json_callback_ = json_callback
+
+        self.bind_ip_ = bind_ip
+        self.bind_port_ = bind_port
+
+        self.json_decoder_ = DataclassJsonDecoder(known_types)
+
+    async def handle_client_(self, reader: asyncio.StreamReader, _):
+        request = ""
+        while True:
+            request += (await reader.read(1024)).decode("utf8")
+            try:
+                j, end = self.json_decoder_.raw_decode(request)
+            except json.JSONDecodeError:
+                continue
+            else:
+                self.json_callback_(j)
+                request = request[end:]
+
+    async def listen(self):
+        server = await asyncio.start_server(
+            self.handle_client_, self.bind_ip_, self.bind_port_
+        )
+        async with server:
+            await server.serve_forever()
