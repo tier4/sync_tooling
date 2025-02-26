b9a5ed287678fa58c56f5011516770eefb3e1dc0
Max SCHMELLER <max.schmeller@tier4.jp>
2025-02-26 18:38:39 +0900

    WIP
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- diag_worker/src/diag_worker/util.py
+++ diag_worker/src/diag_worker/util.py
@@ -1,13 +1,16 @@
 from sync_graph import LinuxClockDeviceId, SystemClockId
 
 from linuxptp_monitor.ethtool_harness import CanonicalizedClock
+from sync_tooling_msgs.clock_id_pb2 import ClockId
 
 
-def linuxptp_to_graph_clock_id(
-    clock_id: CanonicalizedClock, hostname: str
-) -> LinuxClockDeviceId | SystemClockId:
+def linuxptp_to_graph_clock_id(clock_id: CanonicalizedClock, hostname: str) -> ClockId:
     match clock_id:
         case "CLOCK_REALTIME":
-            return SystemClockId(hostname)
+            return ClockId(system_clock_id=SystemClockId(hostname=hostname))
         case int():
-            return LinuxClockDeviceId(hostname, clock_id)
+            return ClockId(
+                linux_clock_device_id=LinuxClockDeviceId(
+                    hostname=hostname, clock_device_number=clock_id
+                )
+            )
