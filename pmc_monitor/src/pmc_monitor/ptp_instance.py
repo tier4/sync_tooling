0bd6ff052eaac0acc3b6bc56848a310bebd69075
Max SCHMELLER <max.schmeller@tier4.jp>
2025-03-18 18:40:15 +0900

    chore: remove dead code
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- pmc_monitor/src/pmc_monitor/ptp_instance.py
+++ pmc_monitor/src/pmc_monitor/ptp_instance.py
@@ -9,14 +9,10 @@ from pmc_monitor.pmc_protocol import (
     TimePropertiesDataSet,
     TimeStatusNp,
 )
-from sync_tooling_msgs.diag_tree import Diagnosable, to_diag_tree
-from sync_tooling_msgs.unknown_pb2 import Unknown
 
 
-@dataclass
-class Unsupported(Diagnosable):
-    def diagnose(self):
-        return to_diag_tree(Unknown(msg="PTP instance does not support this command"))
+class Unsupported:
+    pass
 
 
 @dataclass
