2f9e3c515f12c75e7351a83c1375b8d205c319be
Max SCHMELLER <max.schmeller@tier4.jp>
2025-03-24 18:26:44 +0900

    chore(sync_graph): refactor tests to use fixtures
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

---
+++ sync_graph/tests/conftest.py
@@ -0,0 +1,45 @@
+from typing import Literal
+
+import pytest
+
+from sync_tooling_msgs.clock_id_pb2 import ClockId
+from sync_tooling_msgs.interface_id_pb2 import InterfaceId
+from sync_tooling_msgs.linux_clock_device_id_pb2 import LinuxClockDeviceId
+from sync_tooling_msgs.ptp_clock_id_pb2 import PtpClockId
+from sync_tooling_msgs.sensor_id_pb2 import SensorId
+from sync_tooling_msgs.system_clock_id_pb2 import SystemClockId
+
+
+@pytest.fixture
+def sample_clock_ids() -> (
+    dict[Literal["system", "ptp", "sensor", "iface", "device"], ClockId]
+):
+    return {
+        "system": ClockId(system_clock_id=SystemClockId(hostname="sample")),
+        "ptp": ClockId(ptp_clock_id=PtpClockId(id="012345.fffe.6789ab")),
+        "sensor": ClockId(sensor_id=SensorId(name="my_sensor", ip="192.168.1.201")),
+        "iface": ClockId(
+            interface_id=InterfaceId(hostname="sample", interface_name="eno1")
+        ),
+        "device": ClockId(
+            linux_clock_device_id=LinuxClockDeviceId(
+                hostname="sample", clock_device_number=0
+            )
+        ),
+    }
+
+
+@pytest.fixture
+def nic_clock_ids() -> dict[Literal["device"], ClockId]:
+    return {
+        "device": ClockId(
+            linux_clock_device_id=LinuxClockDeviceId(
+                hostname="sample", clock_device_number=3
+            )
+        )
+    }
+
+
+@pytest.fixture
+def remote_clock_ids() -> dict[Literal["ptp"], ClockId]:
+    return {"ptp": ClockId(ptp_clock_id=PtpClockId(id="010101.fffe.101010"))}
