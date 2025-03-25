269a0e04f33b0486249746a3e23b564eefef7f5c
Max SCHMELLER <max.schmeller@tier4.jp>
2025-03-25 18:44:28 +0900

    chore(diag_master): update import path for SyncGraph
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- diag_master/src/diag_master/echarts_adapter.py
+++ diag_master/src/diag_master/echarts_adapter.py
@@ -1,6 +1,6 @@
 import logging
 
-from sync_graph import SyncGraph
+from sync_graph.sync_graph import SyncGraph
 from sync_tooling_msgs.clock_id import readable_clock_id, readable_clock_type
 from sync_tooling_msgs.clock_id_pb2 import ClockId
 from sync_tooling_msgs.diag_status_pb2 import DiagStatus
