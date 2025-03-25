5b83979275ab04afe0d547aa12184eaa9891947f
Max SCHMELLER <max.schmeller@tier4.jp>
2025-03-25 21:45:20 +0900

    test(sync_graph): refactor to use `_gu` graph update builder
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- sync_graph/tests/test_graph_consistency.py
+++ sync_graph/tests/test_graph_consistency.py
@@ -1,4 +1,3 @@
-from sync_graph import SyncGraph
 from sync_tooling_msgs.clock_alias_update_pb2 import ClockAliasUpdate
 from sync_tooling_msgs.clock_master_update_pb2 import ClockMasterUpdate
 from sync_tooling_msgs.graph_update_pb2 import GraphUpdate
@@ -7,18 +6,11 @@ from sync_tooling_msgs.port_id_pb2 import PortId
 from sync_tooling_msgs.ptp_parent_update_pb2 import PtpParentUpdate
 from sync_tooling_msgs.slave_clock_state_pb2 import SlaveClockState
 
-
-def graph_after_updates(*updates: GraphUpdate):
-    g = SyncGraph()
-    for u in updates:
-        g.update(u)
-    return g
+from .util import _gu, graph_after_updates
 
 
 def test_clock_creation(sample_clock_ids):
-    u = GraphUpdate(
-        clock_master_update=ClockMasterUpdate(clock_id=sample_clock_ids["system"])
-    )
+    u = _gu(ClockMasterUpdate(clock_id=sample_clock_ids["system"]))
     g = graph_after_updates(u)
 
     assert g._graph.number_of_nodes() == 1
@@ -26,16 +18,10 @@ def test_clock_creation(sample_clock_ids):
 
 
 def test_clock_aliases(sample_clock_ids):
-    u1 = GraphUpdate(
-        clock_master_update=ClockMasterUpdate(clock_id=sample_clock_ids["system"])
-    )
-    u2 = GraphUpdate(
-        clock_master_update=ClockMasterUpdate(clock_id=sample_clock_ids["ptp"])
-    )
-    u3 = GraphUpdate(
-        clock_alias_update=ClockAliasUpdate(
-            aliases=[sample_clock_ids["ptp"], sample_clock_ids["system"]]
-        )
+    u1 = _gu(ClockMasterUpdate(clock_id=sample_clock_ids["system"]))
+    u2 = _gu(ClockMasterUpdate(clock_id=sample_clock_ids["ptp"]))
+    u3 = _gu(
+        ClockAliasUpdate(aliases=[sample_clock_ids["ptp"], sample_clock_ids["system"]])
     )
     g = graph_after_updates(u1, u2, u3)
 
@@ -45,14 +31,9 @@ def test_clock_aliases(sample_clock_ids):
 
 def test_alias_precedence(sample_clock_ids):
     updates: list[GraphUpdate] = [
-        GraphUpdate(clock_master_update=ClockMasterUpdate(clock_id=v))
-        for v in sample_clock_ids.values()
+        _gu(ClockMasterUpdate(clock_id=v)) for v in sample_clock_ids.values()
     ]
-    updates.append(
-        GraphUpdate(
-            clock_alias_update=ClockAliasUpdate(aliases=list(sample_clock_ids.values()))
-        )
-    )
+    updates.append(_gu(ClockAliasUpdate(aliases=list(sample_clock_ids.values()))))
     g = graph_after_updates(*updates)
 
     assert g._graph.number_of_nodes() == 1
@@ -66,11 +47,7 @@ def test_ptp_link(sample_clock_ids, remote_clock_ids):
     src = sample_clock_ids["system"]
     dst = remote_clock_ids["ptp"]
 
-    u = GraphUpdate(
-        ptp_parent_update=PtpParentUpdate(
-            clock_id=dst, parent=PortId(clock_id=src, port_number=1)
-        )
-    )
+    u = _gu(PtpParentUpdate(clock_id=dst, parent=PortId(clock_id=src, port_number=1)))
     g = graph_after_updates(u)
 
     assert g._graph.number_of_nodes() == 2
@@ -83,9 +60,7 @@ def test_ptp_link(sample_clock_ids, remote_clock_ids):
 def test_phc2sys_link(sample_clock_ids, nic_clock_ids):
     src = sample_clock_ids["system"]
     dst = nic_clock_ids["device"]
-    u = GraphUpdate(
-        phc2sys_update=Phc2SysUpdate(src=src, dst=dst, clock_state=SlaveClockState())
-    )
+    u = _gu(Phc2SysUpdate(src=src, dst=dst, clock_state=SlaveClockState()))
     g = graph_after_updates(u)
 
     assert g._graph.number_of_nodes() == 2
@@ -103,17 +78,11 @@ def test_alias_after_links(sample_clock_ids, nic_clock_ids, remote_clock_ids):
     dst2 = remote_clock_ids["ptp"]
 
     updates = [
-        GraphUpdate(
-            phc2sys_update=Phc2SysUpdate(
-                src=src1, dst=dst1, clock_state=SlaveClockState()
-            )
+        _gu(Phc2SysUpdate(src=src1, dst=dst1, clock_state=SlaveClockState())),
+        _gu(
+            PtpParentUpdate(clock_id=dst2, parent=PortId(clock_id=src2, port_number=1))
         ),
-        GraphUpdate(
-            ptp_parent_update=PtpParentUpdate(
-                clock_id=dst2, parent=PortId(clock_id=src2, port_number=1)
-            )
-        ),
-        GraphUpdate(clock_alias_update=ClockAliasUpdate(aliases=[src1, src2])),
+        _gu(ClockAliasUpdate(aliases=[src1, src2])),
     ]
 
     g = graph_after_updates(*updates)
