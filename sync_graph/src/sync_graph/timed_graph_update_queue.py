33738e391b2bb1e03344bdfcf00db6c374f1312b
Max SCHMELLER <max.schmeller@tier4.jp>
2025-03-25 18:24:45 +0900

    feat(sync_graph): a timed update queue that drops expired updates
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

---
+++ sync_graph/src/sync_graph/timed_graph_update_queue.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass, field
+from datetime import datetime, timedelta
+
+from sync_tooling_msgs.graph_update_pb2 import GraphUpdate
+
+
+@dataclass
+class GraphUpdateStamped:
+    receive_timestamp: datetime
+    u: GraphUpdate
+
+
+@dataclass
+class TimedGraphUpdateQueue:
+    timeout: timedelta
+
+    _graph_updates: list[GraphUpdateStamped] = field(default_factory=list)
+
+    @property
+    def updates(self):
+        self._drop_expired_updates()
+        return self._graph_updates
+
+    def push(self, u: GraphUpdate):
+        update_stamped = GraphUpdateStamped(datetime.now(), u)
+        self._graph_updates.append(update_stamped)
+
+    def _drop_expired_updates(self):
+        slice_index = 0
+        cutoff_timestamp = datetime.now() - self.timeout
+        for i, update_stamped in enumerate(self._graph_updates):
+            if update_stamped.receive_timestamp > cutoff_timestamp:
+                slice_index = i
+                break
+
+        self._graph_updates = self._graph_updates[slice_index:]
