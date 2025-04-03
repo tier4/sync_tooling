14fa18a052a924554becb84dd72f6641dfdfa98a
Max SCHMELLER <max.schmeller@tier4.jp>
2025-04-03 16:52:35 +0900

    test(sync_graph): more tests for clock diag
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- sync_graph/tests/test_clock_diagnostics.py
+++ sync_graph/tests/test_clock_diagnostics.py
@@ -4,7 +4,13 @@ from sync_tooling_msgs.clock_master_update_pb2 import ClockMasterUpdate
 from sync_tooling_msgs.port_state_pb2 import PortState
 from sync_tooling_msgs.port_state_update_pb2 import PortStateUpdate
 
-from .util import _gu, aggregated_status_label, graph_after_updates
+from .util import (
+    _gu,
+    aggregated_status_label,
+    graph_after_updates,
+    make_measurement,
+    make_phc2sys_link,
+)
 
 
 @pytest.mark.parametrize("clock_setup", ["plain", "ok_port", "faulty_port"])
@@ -34,3 +40,26 @@ def test_single_clock(nic_port_id, clock_setup):
     assert clock_id in g._graph.nodes
     diag_tree = g.diagnose_clock(clock_id)
     assert aggregated_status_label(diag_tree) == "ok"
+
+
+def test_cycle(sample_clock_ids, remote_clock_ids, nic_clock_ids):
+    """
+    All clocks in a cycle shall be diagnosed as `Error`, clocks not in the cycle shall be unaffected.
+    """
+
+    cycle_clock_1 = sample_clock_ids["system"]
+    cycle_clock_2 = nic_clock_ids["device"]
+    unaffected_clock = remote_clock_ids["ptp"]
+
+    us = [
+        make_phc2sys_link(cycle_clock_1, cycle_clock_2, False),
+        make_phc2sys_link(cycle_clock_2, cycle_clock_1, False),
+        make_measurement(cycle_clock_1, unaffected_clock, False),
+    ]
+
+    g = graph_after_updates(*us)
+
+    for cycle_clock in (cycle_clock_1, cycle_clock_2):
+        assert aggregated_status_label(g.diagnose_clock(cycle_clock)) == "error"
+
+    assert aggregated_status_label(g.diagnose_clock(unaffected_clock)) == "ok"
