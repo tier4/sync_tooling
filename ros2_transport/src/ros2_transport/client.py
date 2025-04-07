0ea9028f05f295f9afe31ff21ab08b33b605d8b8
Max SCHMELLER <max.schmeller@tier4.jp>
2025-04-07 19:41:33 +0900

    chore(ros2_transport): simplify by using UInt8MultiArray
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- ros2_transport/src/ros2_transport/client.py
+++ ros2_transport/src/ros2_transport/client.py
@@ -1,17 +1,15 @@
 from rclpy.node import Node
-from std_msgs.msg import String
+from std_msgs.msg import UInt8MultiArray
 
 from sync_tooling_msgs.graph_update_pb2 import GraphUpdate
-
-from .codec import lossless_decode
 
 
 class Ros2Client:
     def __init__(self, topic: str, node: Node):
-        self._publisher = node.create_publisher(String, topic, 10)
+        self._publisher = node.create_publisher(UInt8MultiArray, topic, 10)
 
     def send(self, obj: GraphUpdate):
         serialized = obj.SerializeToString()
-        msg = String()
-        msg.data = lossless_decode(serialized)
+        msg = UInt8MultiArray()
+        msg.data = serialized
         self._publisher.publish(msg)
