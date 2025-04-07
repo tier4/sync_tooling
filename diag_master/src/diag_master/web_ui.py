dbb7c475a290bc31c6ace95a59508051a8a2bd87
Max SCHMELLER <max.schmeller@tier4.jp>
2025-04-07 19:44:01 +0900

    refactor(diag_master): use ros2_transport, use websockets for echart updates, use html templates
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

---
+++ diag_master/src/diag_master/web_ui.py
@@ -0,0 +1,25 @@
+from threading import Thread
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+from diag_master.echarts_adapter import sync_graph_to_echart_options
+from sync_graph.sync_graph import SyncGraph
+
+
+class WebUi:
+    def __init__(self):
+        self._app = Flask(__name__)
+        self._web_socket = SocketIO(self._app)
+
+        @self._app.route("/")
+        def index():
+            return render_template("index.html")
+
+    def update(self, sg: SyncGraph):
+        echart_options = sync_graph_to_echart_options(sg)
+        self._web_socket.emit("update_event", echart_options)
+
+    def run(self):
+        self._thread = Thread(target=self._app.run, args=("0.0.0.0", 5000))
+        self._thread.start()
