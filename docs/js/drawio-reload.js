3686911e658e29d352cdf691db6010581cbd4ef1
Max SCHMELLER <max.schmeller@tier4.jp>
2025-04-09 19:06:55 +0900

    chore(docs): dark mode support
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- docs/js/drawio-reload.js
+++ docs/js/drawio-reload.js
@@ -1,3 +1,29 @@
+function setDrawioDarkMode(enable) {
+  let elements = document.querySelectorAll(".mxgraph");
+  elements.forEach(element => {
+    let data = JSON.parse(element.dataset.mxgraph);
+    data["dark-mode"] = enable ? "dark" : "light";
+    element.dataset.mxgraph = JSON.stringify(data);
+  });
+}
+
+function isPageDark() {
+  return __md_get("__palette").index === 1;
+}
+
 document$.subscribe(({ body }) => {
+  setDrawioDarkMode(isPageDark());
   GraphViewer.processElements()
 })
+
+document.getElementById("__palette_0").addEventListener("change", () => {
+  console.log('Switched to light mode');
+  setDrawioDarkMode(false);
+  GraphViewer.processElements();
+});
+
+document.getElementById("__palette_1").addEventListener("change", () => {
+  console.log('Switched to dark mode');
+  setDrawioDarkMode(true);
+  GraphViewer.processElements();
+});
