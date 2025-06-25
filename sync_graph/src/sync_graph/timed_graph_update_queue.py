d86564e81f110bf111a9ea61bec07efad3556b52
Max SCHMELLER <max.schmeller@tier4.jp>
2025-06-25 17:30:54 +0900

    docs(timed_graph_update_queue): add doc comments
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- sync_graph/src/sync_graph/timed_graph_update_queue.py
+++ sync_graph/src/sync_graph/timed_graph_update_queue.py
@@ -13,14 +13,31 @@ class GraphUpdateStamped:
 
 @dataclass
 class TimedGraphUpdateQueue:
+    """
+    Maintains a queue of graph updates, sorted by arrival time, and limited to a maximum age.
+    This class can be used to instantiate a `SyncGraph` of the system state in the last `timeout`
+    seconds.
+    """
+
     timeout: timedelta
+    """
+    The maximum age of graph updates kept in the queue.
+    """
 
     _graph_updates: list[GraphUpdateStamped] = field(default_factory=list)
 
     @property
-    def updates(self):
+    def updates(self) -> list[GraphUpdate]:
+        """
+        Returns a list of graph updates, sorted by arrival time, and limited to `timeout` age.
+        When called, also drops expired updates.
+        """
+
         self._drop_expired_updates()
 
+        # This optimizes the amount of renaming operations the SyncGraph has to perform.
+        # Alias updates require renaming their affected clocks, ports and references to them,
+        # so they should be processed at a point where there are no references to clocks yet.
         def get_type_precedence(u: GraphUpdate):
             match u.WhichOneof("update"):
                 case "clock_alias_update":
@@ -36,6 +53,12 @@ class TimedGraphUpdateQueue:
         )
 
     def push(self, u: GraphUpdate):
+        """
+        Adds a graph update to the queue, timestamped with the current monotonic time.
+
+        Args:
+            u: The graph update to add.
+        """
         self._drop_expired_updates()
         update_stamped = GraphUpdateStamped(time.monotonic(), u)
         self._graph_updates.append(update_stamped)
