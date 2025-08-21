33dc7baade2d7f7230a4dfc53f66dccb60e4fe6f
Max SCHMELLER <max.schmeller@tier4.jp>
2025-08-21 13:10:05 +0900

    build: workaround for cloudflare pages not having graphviz
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

---
+++ scripts/cloudflare_pages_build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+apt download graphviz
+mkdir graphviz
+dpkg --fsys-tarfile graphviz*.deb |  tar -xf - -C graphviz/
+graphviz_dir="$(realpath graphviz)"
+export PATH="$PATH:$graphviz_dir/usr/bin"
+uv build --all-packages
+uv run mkdocs build --strict
