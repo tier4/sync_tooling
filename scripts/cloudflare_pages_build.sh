ff1cd2255aba60b3fd2d3ede767a2c5da79ba478
Max SCHMELLER <max.schmeller@tier4.jp>
2025-08-21 13:11:34 +0900

    build: fix missing networkx
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- scripts/cloudflare_pages_build.sh
+++ scripts/cloudflare_pages_build.sh
@@ -7,5 +7,6 @@ mkdir graphviz
 dpkg --fsys-tarfile graphviz*.deb |  tar -xf - -C graphviz/
 graphviz_dir="$(realpath graphviz)"
 export PATH="$PATH:$graphviz_dir/usr/bin"
+uv sync --all-packages --all-extras
 uv build --all-packages
 uv run mkdocs build --strict
