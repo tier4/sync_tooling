967c8a98901f2a53708453a3405bdf8168be7843
Max SCHMELLER <max.schmeller@tier4.jp>
2025-08-21 13:16:51 +0900

    build: add missing libgvc6
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- scripts/cloudflare_pages_build.sh
+++ scripts/cloudflare_pages_build.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-apt download graphviz
+apt download graphviz libgvc6
 mkdir graphviz
 dpkg --fsys-tarfile graphviz*.deb |  tar -xf - -C graphviz/
+dpkg --fsys-tarfile libgvc6*.deb |  tar -xf - -C graphviz/
 graphviz_dir="$(realpath graphviz)"
 export PATH="$PATH:$graphviz_dir/usr/bin"
 uv sync --all-packages --all-extras
