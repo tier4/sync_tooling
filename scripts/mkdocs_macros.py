3aac7482a1cccbf7d1db898e6eeeb11434b186c9
Max SCHMELLER <max.schmeller@tier4.jp>
2025-08-21 12:41:43 +0900

    chore: add mkdocs macro to render an apache echart
    Signed-off-by: Max SCHMELLER <max.schmeller@tier4.jp>

--- scripts/mkdocs_macros.py
+++ scripts/mkdocs_macros.py
@@ -1,3 +1,18 @@
+import jinja2
+
+from diag_master.echarts_adapter import sync_graph_to_echart_options
+from sync_graph.sync_graph import SyncGraph
+
+ECHART_TEMPLATE = """
+<script src="https://cdn.jsdelivr.net/npm/echarts@6.0.0/dist/echarts.min.js"></script>
+<div id="sync-doctor-chart" style="width: 100%; aspect-ratio: 16/10;"></div>
+<script defer>
+    var chart = echarts.init(document.getElementById('sync-doctor-chart'));
+    chart.setOption({{echart_options_json}});
+</script>
+"""
+
+
 def define_env(env):
     @env.macro
     def label(label: str):
@@ -24,3 +39,19 @@ def define_env(env):
             label: The label to reference.
         """
         return f"[{label}][{label}]"
+
+    @env.macro
+    def visualize_sync_doctor_echart(generator_module_name: str):
+        import importlib
+        import json
+
+        generator = importlib.import_module(generator_module_name)
+        sg: SyncGraph = generator.generate_graph()
+        echart_options = sync_graph_to_echart_options(sg)
+        echart_options["title"]["show"] = False
+        echart_options["backgroundColor"] = "transparent"
+        echart_options_json = json.dumps(echart_options)
+
+        template = jinja2.Template(ECHART_TEMPLATE)
+        html = template.render(echart_options_json=echart_options_json)
+        return html
